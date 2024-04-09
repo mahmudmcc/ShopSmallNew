da593d795e41d0fe8afed921e3eb06f6a5eaded1
mahmudmcc <mahmudmcc@gmail.com>
2024-04-09 11:27:01 +0600

    Added Product DTO

---
+++ ProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace src.DTOs.Product;
+
+public class ProductDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
